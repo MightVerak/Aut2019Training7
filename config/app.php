9dbdf1daad3e6569434e6b3336cea70f3e2616d3
SirPeterTheBrave <pierre-rafael@outlook.com>
2019-09-06 17:02:39 -0400

    Bake des users

--- config/app.php
+++ config/app.php
@@ -265,7 +265,7 @@ return [
             //'port' => 'non_standard_port_number',
             'username' => 'root',
             'password' => 'mysql',
-            'database' => 'cake_cms_tuto',
+            'database' => 'gestion_formation',
             /*
              * You do not need to set this flag to use full utf-8 encoding (internal default since CakePHP 3.6).
              */
@@ -308,7 +308,7 @@ return [
             //'port' => 'non_standard_port_number',
             'username' => 'root',
             'password' => 'mysql',
-            'database' => 'cake_cms_tuto',
+            'database' => 'gestion_formation',
             //'encoding' => 'utf8mb4',
             'timezone' => 'UTC',
             'cacheMetadata' => true,
